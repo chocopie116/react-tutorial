dc17e58cb7a63c3e3b4d69e9b229f0ac1d8e3852
Tsubasa-Kajinuma <Tsubasa_Kajinuma@voyagegroup.com>
2015-01-29 22:17:03 +0900

    プレーンなdivタグを使って、Reactのcomponentを作成しhtmlにmount(render)した

--- public/scripts/comment.js
+++ public/scripts/comment.js
@@ -1,1 +1,14 @@
-//
+var CommentBox = React.createClass({
+    render: function() {
+        return (
+            <div className="commentBox">
+                Hello React. This is CommentBox
+            </div>
+        );
+    }
+});
+
+React.render(
+    <CommentBox />,
+    document.getElementById('content')
+);
