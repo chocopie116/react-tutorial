0bb9df8c3cf9b6f9938a4579330a3550d4529ff2
Tsubasa-Kajinuma <Tsubasa_Kajinuma@voyagegroup.com>
2015-01-29 22:31:46 +0900

    commentListとcommentFormのcomponentを新規作成

--- public/scripts/comment_simple_div.js
+++ public/scripts/comment_simple_div.js
@@ -7,6 +7,25 @@ var CommentBox = React.createClass({
         );
     }
 });
+var CommentList = React.createClass({
+    render: function() {
+        return (
+            <div className="commentList">
+                This is commentList.
+            </div>
+        );
+    }
+});
+
+var CommentForm = React.createClass({
+    render: function() {
+        return (
+            <div className="commentForm">
+                This is commentForm.
+            </div>
+        );
+    }
+});
 
 React.render(
     <CommentBox />,
